b3f7cd6c2c080e7b2e9b8f8f649b267346def90a
alejandro-vgr <alejandro.valverde.av@gmail.com>
2019-09-30 11:09:13 +0200

    patata

--- asir1.php
+++ asir1.php
@@ -1,7 +1,16 @@
 <a>alejandro</a>
 </br>
+
+<P> Enlace al compañero de la derecha</p>
 <a href="http://192.168.0.82/asir1_saioa/asir1.html">saioa</a>
+
+<p> Enlace al compañero de la izquierda</p>
+<a href="http://192.168.0.74/asir1_victor/asir1.html">victor</a>
+
 <?php
 $f=fopen ('visitas.txt','a');
-fwrite($f,$_SERVER('REMOTE_ADDR']"\r\n");
+fwrite($f,$_SERVER['REMOTE_ADDR']);
+fwrite($f,date(' Y-m-d H:i'));
+fwrite($f,"\r\n"); 
+fclose($f);
 ?>
