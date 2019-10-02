9167df42b58afe695668e68f26801a62cf5ab620
alejandro-vgr <alejandro.valverde.av@gmail.com>
2019-10-02 08:51:52 +0200

    parametros

---
+++ 2bp.php
@@ -0,0 +1,18 @@
+<?php
+function calcula($a,$b)
+{
+echo ($a + $b);
+echo " es suma de $a + $b <br>";
+echo ($a - $b);
+echo " es resta de $a - $b <br>";
+echo ($a * $b);
+echo " es multiplicación de $a * $b <br>";
+echo ($a / $b);
+echo " es división de $a / $b <br>";
+echo "<br>";
+}
+calcula(12,4);
+calcula(35,7);
+calcula(80,8);
+calcula(99,9);
+?>
