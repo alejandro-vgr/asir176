a3ad761a5c6d32378dc4819f08b14e667351eb4f
alejandro-vgr <alejandro.valverde.av@gmail.com>
2019-10-09 10:10:53 +0200

    arrays

---
+++ 2f.php
@@ -0,0 +1,29 @@
+<?php
+$a=['enero',
+'febrero',
+'marzo',
+'abril',
+'mayo',
+'junio',
+'julio',
+'agosto',
+'septiembre',
+'octubre',
+'noviembre',
+'diciembre'
+];
+for ($i=0; $i<12; $i++) 
+    echo $a[$i].'<br>';
+	echo "<br>";	
+	
+
+echo "<br>";
+echo "<br/>";
+	
+echo "<br>";
+	
+foreach ($a as $mes){
+echo $mes."<br/>";
+
+}
+?>
